596d36839dd32a440960723eeedd46f756c6df57
Thamid Miah <thamidmiah05@gmail.com>
2026-02-14 15:26:47 +0000

    Akro wrapper changes

--- garagei/envs/akro_wrapper.py
+++ garagei/envs/akro_wrapper.py
@@ -5,6 +5,10 @@ from garage.envs import EnvSpec
 class AkroWrapperTrait:
     @property
     def spec(self):
+        # If wrapped env already has a spec, use it (for gymnasium compatibility)
+        if hasattr(self.env, 'spec') and hasattr(self.env.spec, 'action_space'):
+            return self.env.spec
+        # Otherwise create spec from gym spaces
         return EnvSpec(action_space=akro.from_gym(self.action_space),
                        observation_space=akro.from_gym(self.observation_space))
 
